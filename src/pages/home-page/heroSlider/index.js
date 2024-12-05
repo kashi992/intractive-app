e85cd7d6f4adeb9f49a3bfc9e1998d379d3e029b
kashi992 <kashifrajput992@gmail.com>
2024-12-06 00:43:33 +1100

    nn

--- src/pages/home-page/heroSlider/index.js
+++ src/pages/home-page/heroSlider/index.js
@@ -22,7 +22,7 @@ import dummy3 from "../../../assets/videos/dumy.mp4";
 import dummy4 from "../../../assets/videos/dumy.mp4";
 import dummy5 from "../../../assets/videos/dumy.mp4";
 import dummy6 from "../../../assets/videos/dumy.mp4";
-import mfv1 from "../../../assets/videos/dharug_country_1.mp4";
+import mfv1 from "../../../assets/videos/dharug_country_2_1.mp4";
 import collabVideo1 from "../../../assets/videos/241204_SydneyMetro_test_v02b.mp4";
 import playBtn from "../../../assets/images/playBtn.png";
 import pdfFile from "../../../assets/images/ExecutiveSummary.pdf"; // PDF file for download
