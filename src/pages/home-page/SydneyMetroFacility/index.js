80784d3e8f4ef5fd6b0f75536d65aa63a0212064
kashi992 <kashifrajput992@gmail.com>
2025-03-17 10:23:58 +0500

    Sydney thumb updated

--- src/pages/home-page/SydneyMetroFacility/index.js
+++ src/pages/home-page/SydneyMetroFacility/index.js
@@ -44,7 +44,7 @@ const SydneyMetroFacility = () => {
                         >
                             <div className="relative mx-auto w-full">
                                 <img
-                                    src="https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/SydneyMetroThumb2.jpg"
+                                    src="https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/SydneyMetroThumb2+(2).jpg"
                                     alt="Thumbnail vision"
                                     className="w-full min-[1680px]:h-[600px] min-[1370px]:h-[450px] md:h-[350px] h-[200px] object-cover"
                                     
@@ -75,7 +75,7 @@ const SydneyMetroFacility = () => {
                             className="w-full h-full custom-video-player"
                             src="https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/%23FlyThrough_ANIMATIC_v2_1.mp4"
                             controls
-                            poster="https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/SydneyMetroThumb2.jpg"
+                            poster="https://cpb-uglsolution-videos.s3-accelerate.amazonaws.com/SydneyMetroThumb2+(2).jpg"
                         />
                         {!isPlaying && (
                             <div className={`absolute top-0 right-0 left-0 bottom-0 h-ull w-full flex flex-col`} onClick={() => videoRef.current.play()}>
